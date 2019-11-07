b02a2b7fa5a89978ec7dff51eb26b40247841a45
Donsworkout <donsdevko@gmail.com>
2019-11-07 17:57:16 +0900

    Add socket canvas rendering

--- src/client/App.js
+++ src/client/App.js
@@ -2,16 +2,82 @@ import React, { Component } from 'react';
 import './app.css';
 import { Button, Card, CardTitle, CardText } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import io from 'socket.io-client'
+
+const serverAddress = "http://localhost:3000"
 
 export default class App extends Component {
-  state = { username: null };
+
+  constructor(props) {
+    super(props);
+    this.display = React.createRef();
+    this.socket = null,
+    this.state = {
+      colors: ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#00ffff', '#ff00ff'],
+      HANDSIZE: 20,
+      HANDCLOSEDCOLOR: "red",
+      HANDOPENCOLOR:"green",
+      HANDLASSOCOLOR: "blue"    
+    }
+  }
+
+  updateHandState(handState, jointPoint) {
+    switch (handState) {
+      case 3:
+        this.drawHand(jointPoint, this.state.HANDCLOSEDCOLOR);
+      break;
+      case 2:
+        this.drawHand(jointPoint, this.state.HANDOPENCOLOR);
+      break;
+      case 4:
+        this.drawHand(jointPoint, this.state.HANDLASSOCOLOR);
+      break;
+    }
+  }
+
+  drawHand(jointPoint, handColor) {
+    // draw semi transparent hand cicles
+    let ctx = this.display.current.getContext('2d');
+    ctx.globalAlpha = 0.75;
+    ctx.beginPath();
+    ctx.fillStyle = handColor;
+    ctx.arc(jointPoint.depthX * 512, jointPoint.depthY * 424, this.stateHANDSIZE, 0, Math.PI * 2, true);
+    ctx.fill();
+    ctx.closePath();
+    ctx.globalAlpha = 1;
+  }
+
+  componentDidMount() {
+    console.log("react ready to go through")
+    let self = this
+
+    self.socket = io(serverAddress);
+    
+    self.socket.on('bodyFrame', bodyFrame => {
+      let ctx = self.display.current.getContext('2d');
+      ctx.clearRect(0, 0, self.display.current.width, self.display.current.height);
+      let index = 0;
+      let colors = self.state.colors;
+
+      bodyFrame.bodies.forEach(function(body){
+        if(body.tracked) {
+          for(let jointType in body.joints) {
+            let joint = body.joints[jointType];
+            ctx.fillStyle = colors[index];
+            ctx.fillRect(joint.depthX * 512, joint.depthY * 424, 10, 10);
+          }
+          //draw hand states
+          self.updateHandState(body.leftHandState, body.joints[7]);
+          self.updateHandState(body.rightHandState, body.joints[11]);
+          index++;
+        }
+      });
+
+    });
+  };
 
   clickRecordButton() {
     fetch('/api/startRecord')
-  }
-
-  clickStopButton() {
-    fetch('/api/stopRecord')
   }
 
   render() {
@@ -25,12 +91,8 @@ export default class App extends Component {
             <div className="col-4"></div>
             <div className="col-4">
               <div className="text-center">
-                <Card body inverse color="primary" className="mb-5">
-                  <CardTitle>키넥트 졸라맨이 들어갈 자리</CardTitle>
-                  <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                </Card>
-                <Button color="danger" className="mr-3" onClick={this.clickRecordButton}>녹화하기</Button>
-                <Button color="primary" onClick={this.clickStopButton}>녹화 종료하기</Button>
+              <canvas className="display" width="512" height="424" ref={this.display}></canvas>
+              <Button color="danger" className="mr-3" onClick={this.clickRecordButton}>녹화하기</Button>
               </div>           
             </div>   
             <div className="col-4"></div>           
--- src/server/index.js
+++ src/server/index.js
@@ -3,16 +3,23 @@ const os = require('os');
 const app = express();
 const Kinect2 = require('kinect2');
 const kinect = new Kinect2();
+const axios = require('axios')
 
 let recordData = ""
+const server = app.listen(process.env.PORT || 3000, () => console.log(`Listening on port ${process.env.PORT || 3000}!`));
+const io = require('socket.io').listen(server);
 
 app.use(express.static('dist'));
 
-app.get('/api/startRecord', (req, res) =>  {
-    console.log("kinect record started");
-    if(kinect.open()) {
-        console.log("Kinect Opened");
-        kinect.on('bodyFrame', function(bodyFrame){
+if(kinect.open()) {
+    console.log("kinect ready to operate");
+
+    // 클라이언트 소켓으로 녹화된 bodyFrame 송신 
+    kinect.on('bodyFrame', function(bodyFrame){
+        io.sockets.emit('bodyFrame', bodyFrame);
+
+         // 녹화 버튼 클릭 (20초간 스쿼트 동작 녹화)
+        app.get('/api/startRecord', (req, res) =>  {
             for(var i = 0;  i < bodyFrame.bodies.length; i++) {
                 if(bodyFrame.bodies[i].tracked) {
                     for(var j = 0;  j < bodyFrame.bodies[i].joints.length; j++) {
@@ -27,18 +34,27 @@ app.get('/api/startRecord', (req, res) =>  {
                     recordData += "\n"
                 }
             }
+            //close the kinect after 5 seconds
+            setTimeout(function(){
+                kinect.removeAllListeners('bodyFrame');
+                kinect.close();
+                console.log("Kinect Closed");
+                recordData = recordData.substring(0, recordData.length - 1);
+
+                axios.post('http://172.30.1.56:5000/analyze_raw', {
+                    data: recordData
+                })
+                .then((res) => {
+                    console.log(res.data)
+                })
+                .catch((error) => {
+                    console.error(error)
+                })
+                
+            }, 20000);            
         });
+	});
 
-        //request body frames
-	    kinect.openBodyReader();
-    }
-});
+    kinect.openBodyReader();
+}
 
-app.get('/api/stopRecord', (req, res) => {
-    kinect.close();
-    recordData = recordData.substring(0, recordData.length - 1);
-    console.log(recordData)
-    console.log("Kinect Closed");
-});
-
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
